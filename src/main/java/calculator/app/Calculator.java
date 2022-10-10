dfaf407ba41291f2ee4b5f769c370ee82b9fcedd
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-10-10 17:09:34 -0400

    made some UI adjustments

--- src/main/java/calculator/app/Calculator.java
+++ src/main/java/calculator/app/Calculator.java
@@ -9,6 +9,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -30,7 +31,7 @@ public class Calculator extends Application {
         genMenuBar();
         genButtons();
 
-        Scene mainScene = new Scene(root, 600, 600);
+        Scene mainScene = new Scene(root, 300, 500);
         mainScene.getStylesheets().add("style.css");
         stage.setScene(mainScene);
         stage.show();
@@ -48,47 +49,79 @@ public class Calculator extends Application {
     private void genButtons(){
         ArrayList<Button> buttons = new ArrayList<>();
         GridPane calcView = new GridPane();
-        calcView.setHgap(2);
-        calcView.setVgap(2);
+        calcView.setHgap(1);
+        calcView.setVgap(1);
         calcView.setAlignment(Pos.CENTER);
-        // when adding to calcView, go by column, row
-        Button plusOrMinus = new Button("±");
-        calcView.add(plusOrMinus, 1, 4);
-        Button zero = new Button("0");
-        calcView.add(zero, 2, 4);
-        Button decimal = new Button(".");
-        calcView.add(decimal, 3, 4);
-        Button equals = new Button("=");
-        calcView.add(equals, 4, 4);
-        Button one = new Button("1");
-        calcView.add(one, 1, 3);
-        Button two = new Button("2");
-        calcView.add(two, 2, 3);
-        Button three = new Button("3");
-        calcView.add(three, 3, 3);
-        Button plus = new Button("+");
-        calcView.add(plus, 4, 3);
-        Button four = new Button("4");
-        calcView.add(four, 1, 2);
-        Button five = new Button("5");
-        calcView.add(five, 2, 2);
-        Button six = new Button("6");
-        calcView.add(six, 3, 2);
-        Button minus = new Button("-");
-        calcView.add(minus, 4, 2);
+        // when adding to calcView, go by column to row
+
+        //row 1
+        Button modulo = new Button("%");
+        calcView.add(modulo, 1, 1);
+        Button clearMostRecent = new Button("CE");
+        calcView.add(clearMostRecent, 2, 1);
+        Button clearAll = new Button ("C");
+        calcView.add(clearAll, 3, 1);
+        Button backSpace = new Button("⇦");
+        calcView.add(backSpace, 4, 1);
+
+        //row 2
+        Button oneOverX = new Button("1/x");
+        calcView.add(oneOverX, 1, 2);
+        Button xPowerTwo = new Button("x²");
+        calcView.add(xPowerTwo, 2, 2);
+        Button squareRoot = new Button("√x");
+        calcView.add(squareRoot, 3, 2);
+        Button divide = new Button("÷");
+        calcView.add(divide, 4, 2);
+
+        //row 3
         Button seven = new Button("7");
-        calcView.add(seven, 1, 1);
+        calcView.add(seven, 1, 3);
         Button eight = new Button("8");
-        calcView.add(eight, 2, 1);
+        calcView.add(eight, 2, 3);
         Button nine = new Button("9");
-        calcView.add(nine, 3, 1);
+        calcView.add(nine, 3, 3);
         Button times = new Button("×");
-        calcView.add(times, 4, 1);
+        calcView.add(times, 4, 3);
+
+        //row 4
+        Button four = new Button("4");
+        calcView.add(four, 1, 4);
+        Button five = new Button("5");
+        calcView.add(five, 2, 4);
+        Button six = new Button("6");
+        calcView.add(six, 3, 4);
+        Button minus = new Button("-");
+        calcView.add(minus, 4, 4);
+
+        //row 5
+        Button one = new Button("1");
+        calcView.add(one, 1, 5);
+        Button two = new Button("2");
+        calcView.add(two, 2, 5);
+        Button three = new Button("3");
+        calcView.add(three, 3, 5);
+        Button plus = new Button("+");
+        calcView.add(plus, 4, 5);
+
+        //row 6
+        Button plusOrMinus = new Button("±");
+        calcView.add(plusOrMinus, 1, 6);
+        Button zero = new Button("0");
+        calcView.add(zero, 2, 6);
+        Button decimal = new Button(".");
+        calcView.add(decimal, 3, 6);
+        Button equals = new Button("=");
+        calcView.add(equals, 4, 6);
+
+
 
         Collections.addAll(buttons, plusOrMinus, zero, decimal, equals,
                                     one, two, three, plus,
                                     four, five, six, minus,
-                                    seven, eight, nine, times);
+                                    seven, eight, nine, times,
+                                    oneOverX, xPowerTwo, squareRoot, divide,
+                                    modulo, clearMostRecent, clearAll, backSpace);
 
         for (Button button : buttons) {
             button.setPrefSize(75, 50); //width, height
